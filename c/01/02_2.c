c43369e730dee2fa980ba4e8b76239159d23337e
RyanJeong <wat6988@gmail.com>
2025-03-09 19:50:14 +0000

    fix: Some codes are added and renamed

---
+++ c/01/02_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* before the function definition */
+int main(/* inside parameter list */ void) /* after parameter list */
+{
+    /* at the beginning of the function body */
+
+    int /* between type and variable name */ x = 0;
+
+    /* before a statement */
+    printf("Hello, ANSI C!\n"); /* after a statement */
+
+    return x; /* after return statement */
+
+} /* after function closing brace */
+
+/* outside the function */
