5e1769e68ac4c5865370fa02ea8ce17c421bd799
RyanJeong <wat6988@gmail.com>
2025-03-17 12:42:07 +0000

    fix: Improved to ignore some codes during compile

--- c/check_syntax.sh
+++ c/check_syntax.sh
@@ -3,6 +3,11 @@ FILE_NAME="error_file_lists.txt"
 MY_FLAGS="-c -fsyntax-only -ansi -Wall -Wextra -Werror -fno-builtin"
 > "$FILE_NAME"
 for file in $(find . -name "*.*" | grep -E "\.c$"); do
+  echo "$file"
+  if [[ "$file" == *"_part"* || "$file" == *"_ignore"* ]]; then
+    echo "  - Skip $file ... "
+    continue
+  fi
   gcc $file $MY_FLAGS > /dev/null 2>&1
   if [ $? -ne 0 ]; then
     echo "$file" >> "$FILE_NAME"
