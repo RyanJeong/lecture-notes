adea7d244354757e7b7d4f2344c8e7b56a13564a
RyanJeong <wat6988@gmail.com>
2025-03-17 12:19:27 +0000

    fix: Added to check code syntax and format codes

---
+++ c/check_syntax.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+FILE_NAME="error_file_lists.txt"
+MY_FLAGS="-c -fsyntax-only -ansi -Wall -Wextra -Werror -fno-builtin"
+> "$FILE_NAME"
+for file in $(find . -name "*.*" | grep -E "\.c$"); do
+  gcc $file $MY_FLAGS > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "$file" >> "$FILE_NAME"
+    gcc $file $MY_FLAGS >> "$FILE_NAME" 2>&1
+    echo "========================================" >> "$FILE_NAME"
+  fi
+done
+
+cat "$FILE_NAME"
