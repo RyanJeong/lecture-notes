4d3a75782fd1a83d74d17603be14710190ae5269
RyanJeong <wat6988@gmail.com>
2025-04-10 19:36:31 +0000

    chore: Modified comments

--- c/02/15.c
+++ c/02/15.c
@@ -4,19 +4,19 @@ int main(void)
 {
     unsigned char x = 248; /* x = 248, 1111 1000 */
 
-    /* mask operation */
-    printf("x & 077 = %d\n", x & 077); /* The result is 0011 1000 */
+    /* mask operation, NB: 077 is an integer constant */
+    printf("x & 077 = %d\n", x & 077); /* x & 077 = 56   (0011 1000) */
 
-    /* set operation */
-    printf("x | 07 = %d\n", x | 07); /* The result is 1111 1111 */
+    /* set operation, NB: 07 is an integer constant */
+    printf("x | 07 = %d\n", x | 07); /* x | 07 = 255   (1111 1111) */
 
     /* advanced mask operation
      *
-     * 0x300 assumes that x is an 8-bit value,
+     * 0300 assumes that x is an 8-bit value,
      * but ~077 works regardless of the word size.
-     * => ~077 is more portable and safer than 0x300
+     * => ~077 is more portable and safer than 0300
      */
-    printf("x & ~077 = %d\n", x & ~077); /* The result is 1100 0000 */
+    printf("x & ~077 = %d\n", x & ~077); /* x & ~077 = 192 (1100 0000) */
 
     return 0;
 }
