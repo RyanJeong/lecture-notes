f7084e99cb81d5e4f2cbb5d95516a6754f831877
RyanJeong <wat6988@gmail.com>
2025-04-20 04:51:08 +0000

    chore: typo

--- c/01/variables_and_arithmetic_expressions_9_ignore.c
+++ c/01/variables_and_arithmetic_expressions_9_ignore.c
@@ -1,3 +1,4 @@
 printf("%d + %d = %d\n", 1, 2, 1 + 2);
 printf("PI: %f\n", 3.141592);
-printf("PI: %d\n", 3.141592);  /* It will print 265890024, not 3 */
+printf("PI: %d\n", 3.141592);  /* prints an indeterminate integer value
+                                  (implementation-defined), not 3 */
