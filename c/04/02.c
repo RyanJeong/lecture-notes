20c70c22ca814ea5ed280311f1c5adcffa360b22
RyanJeong <wat6988@gmail.com>
2025-05-03 09:19:23 +0000

    chore: Modified variable names

--- c/04/02.c
+++ c/04/02.c
@@ -4,21 +4,21 @@
 /* atof: convert string s to double */
 double atof(char s[])
 {
-    double val, power;
+    double digits_value, decimal_scale;
     int i, sign;
 
     for (i = 0; isspace(s[i]); ++i) {} /* skip white space */
     sign = (s[i] == '-') ? -1 : 1;
     if (s[i] == '+' || s[i] == '-')
         ++i;
-    for (val = 0.0; isdigit(s[i]); i++)
-        val = 10.0 * val + (s[i] - '0');
+    for (digits_value = 0.0; isdigit(s[i]); i++)
+        digits_value = 10.0 * digits_value + (s[i] - '0');
     if (s[i] == '.')
         i++;
-    for (power = 1.0; isdigit(s[i]); i++) {
-        val = 10.0 * val + (s[i] - '0');
-        power *= 10;
+    for (decimal_scale = 1.0; isdigit(s[i]); i++) {
+        digits_value = 10.0 * digits_value + (s[i] - '0');
+        decimal_scale *= 10;
     }
 
-    return sign * val / power;
+    return sign * digits_value / decimal_scale;
 }
