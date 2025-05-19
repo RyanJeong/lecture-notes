b291b4b75eb79933b7eeb00140f42784f3f1d003
RyanJeong <wat6988@gmail.com>
2025-05-19 05:23:18 +0000

    chore: typo

--- c/05/function_ptr_cmp.c
+++ c/05/function_ptr_cmp.c
@@ -1,4 +1,4 @@
 void (*func_ptr)(void); /* declaration of a pointer to a function taking void
                            and returning void */
 void *func_ptr(void);   /* declaration of a function taking void and returning
-                       void*   (not a pointer to a function) */
+                           void* (not a pointer to a function) */
