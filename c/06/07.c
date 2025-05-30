90e23bceab4daeffa58b96840b882e47a7877df2
RyanJeong <wat6988@gmail.com>
2025-05-30 19:34:57 +0000

    fix: Modified union handling

--- c/06/07.c
+++ c/06/07.c
@@ -1,15 +1,14 @@
-union {
-    char c;  /* 1 byte */
-    short s; /* 2 bytes */
-    int i;   /* 4 bytes */
-} u;
+#include <stdio.h>
 
 int main(void)
 {
+    union {
+        int i;   /* 4 bytes */
+        short s; /* 2 bytes */
+        char c;  /* 1 byte */
+    } u;
     u.i = 0x12345678;
-
-    &u.s;  /* 0x08AF53DC */
-    u.s;   /* 0x5678 */
+    printf("&u.s: %p, u.s: %x\n", &u.s, u.s); /* &u.s: 0x08AF53DC, u.s: 5678 */
 
     return 0;
 }
