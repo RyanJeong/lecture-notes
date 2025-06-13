14f7f062b191f5233e4c1fe45ab157dcea878906
RyanJeong <wat6988@gmail.com>
2025-06-13 21:48:28 +0000

    add: Added a script to unzip and filter some files

---
+++ assignment/unzip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+find . -name "*.zip" | while IFS= read -r zip_file; do
+  echo "Processing: $zip_file"
+
+  base_name=$(basename "$zip_file" .zip)
+  target_dir=$(echo "$base_name" | grep -oE '[0-9]+' | paste -sd '_')
+  mkdir -p "$target_dir"
+  unzip -q "$zip_file" -d "$target_dir"
+
+  echo "-> Extracted to: $target_dir"
+
+  pushd "$target_dir"
+
+  # 1. *online*
+  rm -rf *online*
+
+  # 2. hidden files (prefix with `.`)
+  find . -name ".*" | while IFS= read -r file; do
+    rm -rf "$file"
+  done
+
+  # 3. print not c extension files
+  find . -type f ! -name "*.c"
+  popd
+
+  echo "-> Successfully filtered unnecessary files"
+done
+
