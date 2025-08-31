1c0db5848511469f5da28dcb40e247eae52730a2
RyanJeong <ryan.m.jeong@hotmail.com>
2025-08-31 16:48:54 +0000

    fix: Update comments in 01_1.c for clarity and consistency

--- c/01/01_1.c
+++ c/01/01_1.c
@@ -1,13 +1,9 @@
-/* 표준 입출력 라이브러리의 함수를 이용 */
-#include <stdio.h>
+#include <stdio.h> /* Use functions from the standard I/O library */
 
-/* 함수 main을 정의, 매개변수는 없다. */
-int main(void)
-/* main 함수 시작 */
+int main(void) /* Define the main function, no parameters */
+               /* Beginning of main function */
 {
-    /* 문자출력을 위해 printf 함수를 호출 */
-    printf("Hello, World\n");
+    printf("Hello, World\n"); /* Call printf function to print a string */
 
-    /* main 함수 종료 */
-    return 0;
+    return 0; /* End of main function */
 }
