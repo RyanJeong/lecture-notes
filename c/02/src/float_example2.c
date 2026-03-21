951dba46922a9be67b21ef664e1a34632de8c675
RyanJeong <ryan.m.jeong@hotmail.com>
2026-03-21 18:21:32 +0000

    style(float_example2.c): correct variable names in printf output for clarity

--- c/02/src/float_example2.c
+++ c/02/src/float_example2.c
@@ -9,7 +9,7 @@ int main(void)
     f5 = 16777217.0; /* Same result as f3, not enough bits to represent f5 */
     f6 = 16777218.0; /* 1.0000 0000 0000 0000 0000 0010 X 2^24 */
     printf("Float type's mantissa has 24 bits (including the hidden bit).\n"
-           "f1: %10.1f, f2: %10.1f, f3: %10.1f, f4: %10.1f\n",
+           "f3: %10.1f, f4: %10.1f, f5: %10.1f, f6: %10.1f\n",
            f3, f4, f5, f6);
 
     return 0;
